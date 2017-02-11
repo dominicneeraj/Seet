49c52069d943193cbd8a5ff7b19b835bc4313b1b
Neeraj <giri492neeraj@gmail.com>
2017-02-11 18:20:13 +0530

    files added

---
+++ app/src/main/java/com/example/neeraj/seet/MainActivity.java
@@ -0,0 +1,239 @@
+package com.example.neeraj.seet;
+
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.gson.Gson;
+import com.microsoft.projectoxford.vision.VisionServiceClient;
+import com.microsoft.projectoxford.vision.VisionServiceRestClient;
+import com.microsoft.projectoxford.vision.contract.AnalysisResult;
+import com.microsoft.projectoxford.vision.contract.Caption;
+import com.microsoft.projectoxford.vision.rest.VisionServiceException;
+import com.example.neeraj.seet.helper.ImageHelper;
+import com.microsoft.speech.tts.Synthesizer;
+import com.microsoft.speech.tts.Voice;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+    private Synthesizer m_syn;
+    // Flag to indicate which task is to be performed.
+    private static final int REQUEST_SELECT_IMAGE = 0;
+
+    // The button to select an image
+    private ImageView mSelectImage;
+
+    // The URI of the image selected to detect.
+    private Uri mImageUri;
+
+    // The image selected to detect.
+    private Bitmap mBitmap;
+
+    // The edit to show status and result.
+    private EditText mEditText;
+    private static final int REQUEST_TAKE_PHOTO = 0;
+    private VisionServiceClient client;
+    private Uri mUriPhotoTaken;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (client==null){
+            client = new VisionServiceRestClient(getString(R.string.subscription_key));
+        }
+        if (m_syn == null) {
+            // Create Text To Speech Synthesizer.
+            m_syn = new Synthesizer("clientid", getString(R.string.text_subscription_key));
+        }
+        m_syn.SetServiceStrategy(Synthesizer.ServiceStrategy.AlwaysService);
+        Voice v = new Voice("en-US", "Microsoft Server Speech Text to Speech Voice (en-US, ZiraRUS)", Voice.Gender.Female, true);
+        //Voice v = new Voice("zh-CN", "Microsoft Server Speech Text to Speech Voice (zh-CN, HuihuiRUS)", Voice.Gender.Female, true);
+        m_syn.SetVoice(v, null);
+
+        // Use a string for speech.
+
+        mSelectImage = (ImageView)findViewById(R.id.selectedImage);
+        mEditText = (EditText)findViewById(R.id.editTextResult);
+    }
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putParcelable("ImageUri", mUriPhotoTaken);
+    }
+
+    // Recover the saved state when the activity is recreated.
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        mUriPhotoTaken = savedInstanceState.getParcelable("ImageUri");
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_describe, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void doDescribe() {
+        mSelectImage.setEnabled(false);
+        mEditText.setText("Describing...");
+
+        try {
+            new doRequest().execute();
+        } catch (Exception e)
+        {
+            mEditText.setText("Error encountered. Exception is: " + e.toString());
+        }
+    }
+
+    // Called when the "Select Image" button is clicked.
+    public void takePhoto(View view) {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if(intent.resolveActivity(getPackageManager()) != null) {
+            // Save the photo taken to a temporary file.
+            File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+            try {
+                File file = File.createTempFile("IMG_", ".jpg", storageDir);
+                mUriPhotoTaken = Uri.fromFile(file);
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, mUriPhotoTaken);
+                startActivityForResult(intent, REQUEST_TAKE_PHOTO);
+            } catch (IOException e) {
+                e.getMessage();
+            }
+        }
+    }
+
+    // Called when image selection is done.
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        Log.d("DescribeActivity", "onActivityResult");
+        switch (requestCode) {
+            case REQUEST_TAKE_PHOTO:
+                if(resultCode == RESULT_OK) {
+                    // If image is selected successfully, set the image URI and bitmap.
+
+                        mImageUri = mUriPhotoTaken;
+
+
+
+                    mBitmap = ImageHelper.loadSizeLimitedBitmapFromUri(
+                            mImageUri, getContentResolver());
+                    if (mBitmap != null) {
+                        // Show the image on screen.
+                        ImageView imageView = (ImageView) findViewById(R.id.selectedImage);
+                        imageView.setImageBitmap(mBitmap);
+
+                        // Add detection log.
+                        Log.d("DescribeActivity", "Image: " + mImageUri + " resized to " + mBitmap.getWidth()
+                                + "x" + mBitmap.getHeight());
+
+                        doDescribe();
+                    }
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+
+    private String process() throws VisionServiceException, IOException {
+        Gson gson = new Gson();
+
+        // Put the image into an input stream for detection.
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        mBitmap.compress(Bitmap.CompressFormat.JPEG, 100, output);
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(output.toByteArray());
+
+        AnalysisResult v = this.client.describe(inputStream, 1);
+
+        String result = gson.toJson(v);
+        Log.d("result", result);
+
+        return result;
+    }
+
+    private class doRequest extends AsyncTask<String, String, String> {
+        // Store error message
+        private Exception e = null;
+
+        public doRequest() {
+        }
+
+        @Override
+        protected String doInBackground(String... args) {
+            try {
+                return process();
+            } catch (Exception e) {
+                this.e = e;    // Store error
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String data) {
+            super.onPostExecute(data);
+            // Display based on error existence
+
+            mEditText.setText("");
+            if (e != null) {
+                mEditText.setText("Error: " + e.getMessage());
+                this.e = null;
+            } else {
+                Gson gson = new Gson();
+                AnalysisResult result = gson.fromJson(data, AnalysisResult.class);
+
+
+
+                for (Caption caption: result.description.captions) {
+                    mEditText.append(caption.text);
+                    Toast.makeText(MainActivity.this, "If the wave is not played, please see the log for more information.", Toast.LENGTH_LONG).show();
+
+
+                    m_syn.SpeakToAudio("hello world");
+                }
+
+
+            }
+
+            mSelectImage.setEnabled(true);
+        }
+    }
+}
